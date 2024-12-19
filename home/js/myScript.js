28edcb002ae7d6f7a464c580ffe8fd6d49cfbfcb
apcsacamama <sacamama@student.apc.edu.ph>
2024-12-20 00:34:02 +0800

    updated script

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML="Paragraph changed.";
 }
+
+function myFunction2() {
+    const element = document.getElementsByTagName("h2");
+
+    document.getElementById("demo").innerHTML = 'h2 The text in first paragraph in (index 0) is: h2' + element[0].innerHTML;
+}
