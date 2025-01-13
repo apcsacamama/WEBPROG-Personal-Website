df57b8385adc07deb23bd39ee7878b0f0bcbfcbd
apcsacamama <sacamama@student.apc.edu.ph>
2025-01-13 19:14:08 +0800

    Update myScript.js

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -2,9 +2,16 @@ function redirectToSocials() {
     window.location.href = "https://frankie-socmeds.carrd.co/";
 }
 
-const app = Vue.createApp({
+const { createApp } = Vue;
+
+createApp({
     data() {
         return {
+            newComment: {
+                name: '',
+                message: ''
+            },
+            comments: [],
             images: [
                 "https://i.pinimg.com/474x/ee/2a/71/ee2a7149341c2b23ae2e9c7358ec247d.jpg",
                 "https://i.pinimg.com/736x/ac/e3/16/ace3163f3edb4b0c311ea0e56bb9d5d1.jpg",
@@ -12,7 +19,17 @@ const app = Vue.createApp({
                 "https://i.pinimg.com/736x/06/ad/da/06adda610b5c8af46f61afa7779760a0.jpg"
             ]
         };
+    },
+    methods: {
+        addComment() {
+            if (this.newComment.name && this.newComment.message) {
+                this.comments.push({
+                    name: this.newComment.name,
+                    message: this.newComment.message
+                });
+                this.newComment.name = '';
+                this.newComment.message = '';
+            }
+        }
     }
-});
-
-app.mount("#gallery");
+}).mount('#app');
